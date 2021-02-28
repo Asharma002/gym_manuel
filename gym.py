a0d5fcd5446036947657b23655199358c5097c6d
Asharma002 <79785668+Asharma002@users.noreply.github.com>
2021-02-28 15:30:19 +0530

    Add files via upload

---
+++ gym.py
@@ -0,0 +1,91 @@
+# GYM
+
+
+# print("press 1 for lock and 2 for retrieve")
+# k = int(input("enter ur choice"))
+# if k==1:
+#   print("press 1 for a , 2 for b and 3 for c")
+#   c = int(input("enter ur choice")) 
+  
+#   if c==1:
+#     print(" press 1  for food or 2 for excercise")
+#     d = int(input("enter ur choice")) 
+#     if d ==1:
+#        with open ("a food.txt","a") as f:
+#          f.write(input("enter ur stuff"))
+#          print("data entered succesfully")
+#     elif d ==2:
+#        with open ("a exc.txt","a") as f:
+#          f.write(input("enter ur stuff"))
+#          print("data entered succesfully")
+
+#   elif c==2:
+#     print(" press 1  for food or 2 for excercise")
+#     d = int(input("enter ur choice")) 
+#     if d ==1:
+#            with open ("b food.txt","a") as f:
+#              f.write(input("enter ur stuff"))
+#              print("data entered succesfully")
+             
+#     elif d ==2:
+ 
+#             with open ("b exc.txt","a") as f:
+#               f.write(input("enter ur stuff"))
+#               print("data entered succesfully")
+ 
+ 
+ 
+#   elif c==3:
+#     print(" press 1  for food or 2 for excercise")
+#     d = int(input("enter ur choice")) 
+#     if d ==1:
+#             with open ("c food.txt","a") as f:
+#               f.write(input("enter ur stuff"))
+#               print("data entered succesfully")
+#     elif d ==2:
+#              with open ("c exc.txt","a") as f:
+#                f.write(input("enter ur stuff"))
+#                print("data entered succesfully")
+# elif k==2:
+#   print("press 1 for a , 2 for b and 3 for c")
+#   c = int(input("enter ur choice")) 
+#   if c==1:
+#     print(" press 1  for food or 2 for excercise")
+#     d = int(input("enter ur choice")) 
+#     if d ==1:
+#        with open ("a food.txt","r") as f:
+#          a= f.read()
+#          print(a)
+#     elif d ==2:
+#        with open ("a exc.txt","r") as f:
+#           b = f.read()
+#           print(b)
+#   elif c==2:
+#      print(" press 1  for food or 2 for excercise")
+#      d = int(input("enter ur choice")) 
+#      if d ==1:
+#          with open ("b food.txt","r") as f:
+#            m =f.read()
+#            print(m)
+           
+#      elif d ==2:
+#           with open ("b exc.txt","r") as f:
+#             n=f.read()
+#             print(n)
+        
+          
+#   elif c==3:
+#      print(" press 1  for food or 2 for excercise")
+#      d = int(input("enter ur choice")) 
+#      if d ==1:
+#           with open ("c food.txt","r") as f:
+#             l =  f.read()
+#             print(l)
+#      elif d ==2:
+#            with open ("c exc.txt","r") as f:
+#              p=f.read()
+#              print(p)
+# else:
+#   print("you have entered wrong input") 
+
+# exit()
